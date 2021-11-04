d9258cd9bbad1a3799a210e123eab1af42e0538b
pierwill <19642016+pierwill@users.noreply.github.com>
2021-11-04 11:04:20 -0500

    fix: Update libflux/flux-core/src/bin/fluxdoc.rs
    Co-authored-by: Markus Westerlind <mwesterlind@influxdata.com>

--- libflux/flux-core/src/bin/fluxdoc.rs
+++ libflux/flux-core/src/bin/fluxdoc.rs
@@ -237,7 +237,7 @@ impl<'a> example::Executor for CLIExecutor<'a> {
 // high standard going forward.
 //
 // See https://github.com/influxdata/flux/issues/4141 for tacking removing of this list.
-const EXCEPTIONS: [&str; 94] = [
+const EXCEPTIONS: &[&str] = &[
     "array",
     "contrib",
     "contrib/RohanSreerama5",
